f0fb19afc14783928edc80f088e7dde15c47e6e5
Robert Rodrigues <robert.rodrigues@it-eam.com>
2022-01-15 17:53:04 -0300

    first commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color deepBlue = Color.fromRGBO(0, 52, 97, 1);
+  static const Color lightBlue = Color.fromRGBO(0, 131, 202, 1);
+  static const Color jadeGreen = Color.fromRGBO(0, 175, 173, 1);
+  static const Color orange = Color.fromRGBO(242, 101, 34, 1);
+  static const Color yellow = Color.fromRGBO(252, 175, 23, 1);
+}
